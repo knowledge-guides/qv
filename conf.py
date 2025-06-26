9a7ce183015a3e1cf405d2385cebfadf17459c1a
knowledge-guides <sunio121045@gmail.com>
2025-06-26 14:09:11 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -32,7 +32,7 @@ html_favicon = 'favicon.ico'
 
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
